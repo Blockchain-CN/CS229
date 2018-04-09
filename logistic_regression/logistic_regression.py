e9e4d94bcdb286213dfb047d33f1ba998abd5f6e
luda <luda@didichuxing.com>
2018-04-09 21:20:56 +0800

    update

--- logistic_regression/logistic_regression.py
+++ logistic_regression/logistic_regression.py
@@ -1,13 +1,14 @@
 # coding=utf-8
 
 """
-20180405: Luda
+https://www.cnblogs.com/sumai/p/5221067.html
 """
 
 import numpy as np
 import pandas as pd
 from numpy import dot
 from numpy.linalg import inv
+import matplotlib.pyplot as plt
 
 iris = pd.read_csv('iris.csv')
 dummy = pd.get_dummies(iris['Species'])
@@ -26,7 +27,7 @@ temp['x0'] = 1
 X = temp.iloc[:, [1, 0]]
 Y = iris['setosa'].reshape(len(iris), 1)
 
-# 批量梯度下降
+# 批量梯度上升降
 m, n  = X.shape
 alpha = 0.0065
 theta_g = np.zeros((n,1))
@@ -40,4 +41,21 @@ for i in range(maxCycles):
     grad = dot(X.T, error) #梯度
     theta_g -= alpha * grad
 print theta_g
-print J[-5:]
+
+
+# newton method
+theta_n = np.zeros((n,1))
+maxCycles = 10
+C = pd.Series(np.arange(maxCycles, dtype = float)) #损失数值
+for i in range(maxCycles):
+    h = logit(dot(X, theta_n)) #估计值
+    C[i] = -(1/100.)*np.sum(Y*np.log(h)+(1-Y)*np.log(1-h)) #计算损失函数值
+    error = h - Y #误差
+    grad = dot(X.T, error) #梯度
+    A = h*(1-h)* np.eye(len(X))
+    H = np.mat(X.T)*A*np.mat(X) #海瑟矩阵, H = X`AX
+    theta_n -= inv(H)*grad
+print theta_n
+C.plot()
+
+plt.show()
